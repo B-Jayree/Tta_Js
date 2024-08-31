6acbd1700349713f08f90fb893b7c4ef26c8f59f
B-Jayree <benybwitonzi@gmail.com>
2024-08-31 15:55:12 +0200

    Week 9 day 4
    comprehended javascript and node.js

---
+++ intro.js
@@ -0,0 +1,11 @@
+let width = 4 ;
+let height = 5 ;
+
+console.log(` width: ${width} cm`)
+console.log(`height: ${height} cm`)
+
+let area = width * height ;
+console.log(`area: ${area} cm2`)
+
+let perimeter = 2 * (width + height) ;
+console.log(`perimeter: ${perimeter} cm`)
