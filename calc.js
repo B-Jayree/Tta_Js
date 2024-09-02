8db7c7576ae28f83576645d0813b7372b97090fa
B-Jayree <benybwitonzi@gmail.com>
2024-09-02 04:07:40 +0200

    understood js functions

---
+++ calc.js
@@ -0,0 +1,18 @@
+// addition funtion
+ const add = (a,b) => a + b ;
+ //substraction function
+ const substract = (a,b) => a - b ;
+ //multiply function
+ const multiply = (a,b) => a * b ;
+ //divide function
+ const divide = (a,b) => a / b ;
+
+ let sum = add(6,3) ;
+ let difference= substract(6,3);
+ let product = multiply(6,3);
+ let quotient = divide(6,3);
+
+ console.log(`The sum is ${sum}`);
+ console.log(`The difference is ${difference}`);
+ console.log(`The product is ${product}`);
+ console.log(`The quotient is ${quotient}`);
